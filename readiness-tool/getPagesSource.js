19a4b297538f12f67bc0cd3fdb7e604ae1accaf9
Alanna Walton <alwalton@google.com>
2017-07-20 14:59:15 -0700

    Cleaned up getPagesSource file

--- readiness-tool/getPagesSource.js
+++ readiness-tool/getPagesSource.js
@@ -1,35 +1,10 @@
 /**
  * Makes source code of the page into a string
- * @param {Object} documentRoot - DOM of page
+ * @param {Document} documentRoot - DOM of page
  * @return {String} 
  */
 function DOMtoString(documentRoot) {
-    var html = '',
-        node = document_root.firstChild;
-    while (node) {
-        switch (node.nodeType) {
-        case Node.ELEMENT_NODE:
-            html += node.outerHTML;
-            break;
-        case Node.TEXT_NODE:
-            html += node.nodeValue;
-            break;
-        case Node.CDATA_SECTION_NODE:
-            html += '<![CDATA[' + node.nodeValue + ']]>';
-            break;
-        case Node.COMMENT_NODE:
-            html += '<!--' + node.nodeValue + '-->';
-            break;
-        case Node.DOCUMENT_TYPE_NODE:
-            // (X)HTML documents are identified by public identifiers
-            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + 
-                                                node.publicId + '"' : '') + 
-              (!node.publicId && node.systemId ? ' SYSTEM' : '') + 
-              (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-            break;
-        }
-        node = node.nextSibling;
-    }
+    var html = documentRoot.documentElement.innerHTML;
     return html;
 }
 
