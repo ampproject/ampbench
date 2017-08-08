ff12a7d428c9e783f3d81bca6e96d006fa1af6b6
Alanna Walton <alwalton@google.com>
2017-08-07 17:46:06 -0700

    moved testing to different repo. This is a filler

--- tests/popup-test.js
+++ tests/popup-test.js
@@ -13,39 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require("../readiness-tool/popup");
-describe('isSupported(key)', function () {
-  it('should return true when the key given is in the supported list', function () {
-    expect(self.popups.isSupported('Adhese')).to.be.true;
-  });
-  it('should return false when the key given is in NOT in the supported list', function () {
-    expect(self.popups.isSupported('alannalytics')).to.be.false;
-  });
-});
-describe('addToDict(tempScript, htmlString, foundThis, key, category)', function () {
-  var htmlString = "candycanes";
-  var foundThis = {
-    'supported': {
-      'ads': []
-      , 'analytics': []
-    }
-    , 'notSupported': {
-      'ads': []
-      , 'analytics': []
-    }
-  };
-      
-  it('should push analytics to the analytics array', function () {
-    var tempScript = "candy";
-    var category = "10";
-    const key = "Candy";
-    self.popups.addToDict(tempScript, htmlString, foundThis, key, category);
-    expect(foundThis.supported.analytics).to.equal(['Candy']);
-    var tempScript = "peppermint";
-    expect(foundThis.notSupported.analytics).to.equal(['Candy']);
-  });
-  
-  it('should return false when the key given is in NOT in the supported list', function () {
-    expect(self.popups.isSupported('alannalytics')).to.be.false;
-  });
-});
+
+describe('test', function() {
+
+   it('should pass', function() {
+     expect(1).to.equal(1);
+   });
+ });
+
+describe('isSupported(key)', function() {
+
+   it('should return true when the key given is in the supported list', function() {
+     expect(isSupported("Adhese")).to.equal(1);
+   });
+ });
