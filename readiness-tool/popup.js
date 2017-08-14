d16345e17a57356afb43b9e834105028bd770334
Alanna Walton <alwalton@google.com>
2017-08-14 12:55:16 -0700

    changed naming

--- readiness-tool/popup.js
+++ readiness-tool/popup.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview Entry point for ART Chrome extension. 
- * Scans the current page against the vendors in the app.json file and
+ * Scans the current page against the vendors in the vendors.json file and
  * specifies them as either supported or not supported.
  */
 self.popups = {};
@@ -28,7 +28,7 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action == 'getSource') {
     // htmlOfTab contains the page source as a string
     htmlOfTab = request.source;
-    findDetectedApps(htmlOfTab);
+    findDetectedVendors(htmlOfTab);
   }
 });
 window.onload = function onWindowLoad() {
@@ -50,43 +50,43 @@ window.onload = function onWindowLoad() {
  * @param {string} html - String containing all HTML on the page
  * @return {Object} 
  */
-function findDetectedApps(html) {
+function findDetectedVendors(html) {
   fetch('vendors.json').then(function (response) {
       response.json().then(function (data) {
-        let listAllApps = data.apps;
-        detectedApps = filteredApps(html, listAllApps);
-        showSupportedAppsInView(detectedApps, listAllApps);
-        return detectedApps;
+        let listAllVendors = data.vendors;
+        detectedVendors = filteredVendors(html, listAllVendors);
+        showSupportedVendorsInView(detectedVendors, listAllVendors);
+        return detectedVendors;
       });
     }).then(data => console.log('data is', data))
     .catch(error => console.log('error is', error))
 }
 /**
  * Add supported and unsupported applications to the view
- * @param {Object} detectedApps - All 3rd Party Applications found on page
- * @param {!Object} listAllApps - JSON of all 3p vendors
+ * @param {Object} detectedVendors - All 3rd Party Applications found on page
+ * @param {!Object} listAllVendors - JSON of all 3p vendors
  */
-function showSupportedAppsInView(detectedApps, listAllApps) {
+function showSupportedVendorsInView(detectedVendors, listAllVendors) {
   supportedAds.textContent = supportedAnalytics.textContent =
     notSupportedAds.textContent = notSupportedAnalytics.textContent =
     blankMessage;
-  supportedAds.appendChild(makeList(detectedApps.supported.ads, false,
-    listAllApps));
-  supportedAnalytics.appendChild(makeList(detectedApps.supported.analytics,
-    false, listAllApps));
-  notSupportedAds.appendChild(makeList(detectedApps.notSupported.ads, true,
-    listAllApps));
+  supportedAds.appendChild(makeList(detectedVendors.supported.ads, false,
+    listAllVendors));
+  supportedAnalytics.appendChild(makeList(detectedVendors.supported.analytics,
+    false, listAllVendors));
+  notSupportedAds.appendChild(makeList(detectedVendors.notSupported.ads, true,
+    listAllVendors));
   notSupportedAnalytics.appendChild(makeList(
-    detectedApps.notSupported.analytics,
-    true, listAllApps));
+    detectedVendors.notSupported.analytics,
+    true, listAllVendors));
 }
 /**
- * Splits all detected apps into 'supported' and 'not supported'
+ * Splits all detected vendors into 'supported' and 'not supported'
  * @param {string} htmlString - String containing all HTML on the page
- * @param {!Object} listAllApps - JSON of all the 3p vendors 
+ * @param {!Object} listAllVendors - JSON of all the 3p vendors 
  * @return {Object} 
  */
-function filteredApps(htmlString, listAllApps) {
+function filteredVendors(htmlString, listAllVendors) {
   const foundThis = {
     'supported': {
       'ads': [],
@@ -97,55 +97,55 @@ function filteredApps(htmlString, listAllApps) {
       'analytics': []
     }
   };
-  // for all the app objects in the vendors.json file
-  Object.keys(listAllApps).forEach(function (appName) {
-    const appConfig = listAllApps[appName];
+  // for all the vendor objects in the vendors.json file
+  Object.keys(listAllVendors).forEach(function (vendorName) {
+    const vendorConfig = listAllVendors[vendorName];
     // If object has a 'uniqueRegExp' key
-    if (appConfig.uniqueRegExp) {
-      appConfig.uniqueRegExp.forEach(function (x) {
-        if (appConfig.category.length == 0) {
-          console.error('The app', appName,
+    if (vendorConfig.uniqueRegExp) {
+      vendorConfig.uniqueRegExp.forEach(function (x) {
+        if (vendorConfig.category.length == 0) {
+          console.error('The vendor', vendorName,
             'does not have a value for "category" in vendors.json');
           return;
-        } else if (appConfig.category != "Ads" && appConfig.category != "Analytics") {
-          console.error('The app',
-            appName,
+        } else if (vendorConfig.category != "Ads" && vendorConfig.category != "Analytics") {
+          console.error('The vendor',
+            vendorName,
             'is not declared as an ads or analytics vendor in vendors.json');
           return;
         }
-        addToDict(x, htmlString, foundThis, appName,
-          appConfig.category[0]);
+        addToDict(x, htmlString, foundThis, vendorName,
+          vendorConfig.category[0]);
       });
     }
   });
   return foundThis;
 }
 /**
- * Pushes app names to the supported or not supported list of the object
+ * Pushes vendor names to the supported or not supported list of the object
  'found this'
  * @param {string} regexString - String representation of regular expression
  * @param {!string} htmlString - String containing all HTML on the page
  * @param {Object} foundThis - Object separating the 3P services by support
- * @param {string} appName - name of third party service
+ * @param {string} vendorName - name of third party service
  * @param {string} category - the category that the key belongs to
  */
-function addToDict(regexString, htmlString, foundThis, appName, category) {
+function addToDict(regexString, htmlString, foundThis, vendorName, category) {
   const regX = new RegExp(regexString);
   if (regX.test(htmlString)) {
-    if (isAppNameUnique(foundThis, appName)) {
+    if (isVendorNameUnique(foundThis, vendorName)) {
       switch (category) {
         case 'Analytics':
-          if (isSupported(appName)) {
-            foundThis.supported.analytics.push(appName);
+          if (isSupported(vendorName)) {
+            foundThis.supported.analytics.push(vendorName);
           } else {
-            foundThis.notSupported.analytics.push(appName);
+            foundThis.notSupported.analytics.push(vendorName);
           }
           break;
         case 'Ads':
-          if (isSupported(appName)) {
-            foundThis.supported.ads.push(appName);
+          if (isSupported(vendorName)) {
+            foundThis.supported.ads.push(vendorName);
           } else {
-            foundThis.notSupported.ads.push(appName);
+            foundThis.notSupported.ads.push(vendorName);
           }
           break;
       }
@@ -153,23 +153,23 @@ function addToDict(regexString, htmlString, foundThis, appName, category) {
   }
 }
 /**
- * Checks to see if appName is unique within the object
+ * Checks to see if vendorName is unique within the object
  * @param {Object} obj - Object separating the 3p services by support
- * @param {string} appName - name of third party service
+ * @param {string} vendorName - name of third party service
  * @return {boolean} 
  */
-function isAppNameUnique(obj, appName) {
+function isVendorNameUnique(obj, vendorName) {
   let count = 0;
-  if (obj.supported.ads.includes(appName)) {
+  if (obj.supported.ads.includes(vendorName)) {
     count ++;
   }
-  if (obj.supported.analytics.includes(appName)) {
+  if (obj.supported.analytics.includes(vendorName)) {
     count ++;
   }
-  if (obj.notSupported.ads.includes(appName)) {
+  if (obj.notSupported.ads.includes(vendorName)) {
     count ++;
   }
-  if (obj.notSupported.analytics.includes(appName)) {
+  if (obj.notSupported.analytics.includes(vendorName)) {
     count ++;
   }
   
@@ -177,11 +177,11 @@ function isAppNameUnique(obj, appName) {
 }
 /**
  * TODO (alwalton@): get list of supported ads/analytics programatically
- * Check if app is in supported list of app names
- * @param {string} appName - name of app
+ * Check if vendor is in supported list of vendor names
+ * @param {string} vendorName - name of vendor
  * @return {boolean} 
  */
-function isSupported(appName) {
+function isSupported(vendorName) {
   const ampSupported = [
     'A8', 'A9', 'AcccessTrade', 'Adblade', 'Adform', 'Adfox', 'Ad Generation',
     'Adhese', 'ADITION', 'Adman', 'AdmanMedia', 'AdReactor', 'AdSense',
@@ -213,17 +213,17 @@ function isSupported(appName) {
     'Quantcast Measurement', 'Segment', 'SOASTA mPulse', 'SimpleReach',
     'Snowplow Analytics', 'Webtrekk', 'Yandex Metrica'
   , ];
-  // If it is NOT in list of supported apps
-  return ampSupported.includes(appName);
+  // If it is NOT in list of supported vendors
+  return ampSupported.includes(vendorName);
 }
 /**
- * Make list of supported/unsupported apps into an unordered list
- * @param {!Array<string>} array - array of app names
+ * Make list of supported/unsupported vendors into an unordered list
+ * @param {!Array<string>} array - array of vendor names
  * @param {boolean} allowToolTips - check to see if tooltip allowed
- * @param {!Object} listAllApps - JSON of all 3p vendors
+ * @param {!Object} listAllVendors - JSON of all 3p vendors
  * @return {e} 
  */
-function makeList(array, allowToolTips, listAllApps) {
+function makeList(array, allowToolTips, listAllVendors) {
   // Create the list element:
   const list = document.createElement('ul');
   for (let i = 0; i < array.length; i++) {
@@ -231,10 +231,10 @@ function makeList(array, allowToolTips, listAllApps) {
     let item = document.createElement('li');
     // Set its contents:
     item.appendChild(document.createTextNode(array[i]));
-    // Tooltip is only allowed for unsupported venodrs
-    if (allowToolTips && listAllApps[array[i]].tooltip != null) {
+    // Tooltip is only allowed for unsupported vendors
+    if (allowToolTips && listAllVendors[array[i]].tooltip != null) {
       item.className = 'tooltip';
-      item.setAttribute("data-tooltip", listAllApps[array[i]].tooltip);
+      item.setAttribute("data-tooltip", listAllVendors[array[i]].tooltip);
     }
     // Add it to the list:
     list.appendChild(item);
