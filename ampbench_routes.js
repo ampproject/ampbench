6eb1b564c5f441badb1becb3cba7b1b409d2678e
Damien Allison <damienkallison@gmail.com>
2018-08-23 10:53:29 +0100

    Changed the signature of the helper to use the rendering context conditional on the environment configuration to trigger including gtag. It could be argued that the helper could be included without the gtag configuration.

--- ampbench_routes.js
+++ ampbench_routes.js
@@ -131,12 +131,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const handlebars = require('handlebars');
 const index_template = fs.readFileSync(__dirname + '/views/index.hbs', 'utf8');
 const results_template = fs.readFileSync(__dirname + '/views/results.hbs', 'utf8');
-if (process.env.GTAG_ID) {
-    console.log('Registering gtag: ' + process.env.GTAG_ID);
-    handlebars.registerHelper('gtag_id', function() {
-        return process.env.GTAG_ID;
-    });
-}
+// Initialise handlebars helper to include gtag
+handlebars.registerHelper('gtag', function(options) {
+    if (process.env.GTAG_ID) {        
+        return options.fn({gtag_id: process.env.GTAG_ID});
+    }
+});
+
 // TODO: WIP20160426 - bulk support routes
 // const multi_url_template = fs.readFileSync(__dirname + '/views/multi_url.hbs', 'utf8');
 
